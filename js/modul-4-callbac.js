96649381f6cc2997c973a616d09f0ef8cf72236a
foton-1 <ovf0675@outlook.com>
2023-04-24 01:10:59 +0300

    modul-4_48/48

--- js/modul-4-callbac.js
+++ js/modul-4-callbac.js
@@ -353,5 +353,242 @@
 // const booksByAuthor = books.filter((bookAutor) => bookAutor.author === AUTHOR);
 // --Значення змінної topRatedBooks - це масив книг з рейтингом, вищим за 8
 
-// ---------------------22/48----
+// ---------------------22/48----filter()
 //
+// const getUsersWithEyeColor = (users, color) => {
+//   return users.filter((user) => user.eyeColor === color);
+// };
+
+// ---------------------23/48----filter()
+// const getUsersWithAge = (users, minAge, maxAge) => {
+//   return users.filter((user) => minAge <= user.age && user.age <= maxAge);
+// };
+
+// ---------------------24/48----filter()--some()
+// const getUsersWithFriend = (users, friendName) => {
+//   return users.filter((user) =>
+//     user.friends.some((element) => element === friendName)
+//   );
+// };
+
+// ---------------------25/48--------flatMap-filter-indexOf
+// const getFriends = (users) => {
+//   const allFriends = users.flatMap((friend) => friend.friends);
+//   return allFriends.filter(
+//     (frend, index, array) => array.indexOf(frend) === index
+//   );
+// };
+
+// ---------------------26/48-------filter()
+// const getActiveUsers = (users) => {
+//   return users.filter((user) => user.isActive === true);
+// };
+
+// ---------------------27/48-------filter()
+// const getInactiveUsers = (users) => {
+//   return users.filter((user) => user.isActive === false);
+// };
+
+// ---------------------28/48-------find()
+// const bookWithTitle = books.find((titl) => titl.title === BOOK_TITLE);
+// const bookByAuthor = books.find((authorFind) => authorFind.author === AUTHOR);
+
+// ---------------------29/48-------find
+// const getUserWithEmail = (users, email) => {
+//     return users.find((user) => user.email === email)
+// };
+
+// --------------------30/48-----every()
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change code below this line
+// const eachElementInFirstIsEven = firstArray.every((even) => even % 2 ===0);
+// const eachElementInFirstIsOdd = firstArray.every((odd) => odd % 2 !==0);
+// const eachElementInSecondIsEven = secondArray.every((evenSec) => evenSec % 2 ===0);
+// const eachElementInSecondIsOdd = secondArray.every((oddSec) => oddSec % 2 !==0);
+// const eachElementInThirdIsEven = thirdArray.every((evenThir) => evenThir %2 ===0);
+// const eachElementInThirdIsOdd = thirdArray.every((oddThir) => oddThir % 2 !==0);
+
+// --------------------31/48-----every()
+// --Робить перевірку і повертає  true або false.
+// const isEveryUserActive = (users) => {return users.every((user) => user.isActive === true)
+// };
+
+// --------------------32/48-----some()
+//--перевіряє, чи проходить хоча б один елемент масиву тест колбек-функції. Повертає true або false
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change below this line
+// const anyElementInFirstIsEven = firstArray.some((evenF) => evenF % 2 ===0);
+// const anyElementInFirstIsOdd = firstArray.some((oddF) => oddF % 2 !==0);
+// const anyElementInSecondIsEven = secondArray.some((evenS) => evenS %2 ===0);
+// const anyElementInSecondIsOdd = secondArray.some((oddS) => oddS % 2!==0);
+// const anyElementInThirdIsEven = thirdArray.some((evenT) => evenT %2 ===0);
+// const anyElementInThirdIsOdd = thirdArray.some((oddT) => oddT %2 !==0);
+
+// --------------------33/48-----some()
+// const isAnyUserActive = users => {return users.some((user) => user.isActive === true)
+// };
+
+// --------------------34/48-----reduce()
+// const players = {
+//   mango: 1270,
+//   poly: 468,
+//   ajax: 710,
+//   kiwi: 244
+// };
+// const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// // Change code below this line
+// const totalPlayTime = playtimes.reduce((totalValue, num) => {return totalValue + num;},0);
+// // Change code above this line
+// const averagePlayTime = totalPlayTime / playtimes.length;
+
+ // --------------------35/48-----reduce()
+// const players = [
+//   { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+//   { name: "Poly", playtime: 469, gamesPlayed: 2 },
+//   { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+//   { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+// const totalAveragePlaytimePerGame = players.reduce((total, time) => {
+//     return total + (time.playtime / time.gamesPlayed);}, 0);
+
+//  --------------------36/48-----reduce()
+// const calculateTotalBalance = users => { 
+//   return users.reduce((total, element) => {
+//     return total + element.balance;}, 0);
+   
+// };
+
+//  --------------------37/48-----reduce()+length
+// const getTotalFriendCount = users => { 
+//   return users.reduce((total, user) => {
+//     return total + user.friends.length;}, 0);
+   
+// };
+
+//  --------------------38/48-----sort()
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//   "Tanith Lee",
+//   "Bernard Cornwell",
+//   "Robert Sheckley",
+//   "Fyodor Dostoevsky",
+// ];
+// // Change code below this line
+// const ascendingReleaseDates = [...releaseDates].sort();
+// const alphabeticalAuthors = [...authors].sort();
+
+//  --------------------39/48-----sort(a,b)
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// // Change code below this line
+// const ascendingReleaseDates = [...releaseDates].sort();
+// const descendingReleaseDates = [...releaseDates].sort((a, b) => b-a);
+
+//  --------------------40/48-----sort(a,b)-localeCompare
+// const authors = [
+//   "Tanith Lee",
+//   "Bernard Cornwell",
+//   "Robert Sheckley",
+//   "Fyodor Dostoevsky",
+//   "Howard Lovecraft",
+// ];
+// // Change code below this line
+// const authorsInAlphabetOrder = [...authors].sort();
+// const authorsInReversedOrder = [...authors].sort((a,b) => b.localeCompare(a));
+
+//  --------------------41/48-----sort(a,b)-localeCompare
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
+// const sortedByAuthorName = [...books].sort((aAut, bAut) => aAut.author.localeCompare(bAut.author));
+// const sortedByReversedAuthorName = [...books].sort((aAutRev, bAutRev) => bAutRev.author.localeCompare(aAutRev.author));
+// const sortedByAscendingRating = [...books].sort((aRat, bRat) => aRat.rating - bRat.rating);
+// const sortedByDescentingRating = [...books].sort((aRatDes, bRatDes) => bRatDes.rating - aRatDes.rating);
+
+// --------------------42/48-----sort(a,b)-
+// const sortByAscendingBalance = users => {return [...users].sort((a, b) => a.balance - b.balance)
+   
+// };
+
+// --------------------43/48-----sort(a,b)+length
+// const sortByDescendingFriendCount = users => {return [...users].sort((a,b) => b.friends.length - a.friends.length)
+   
+// };
+
+
+// --------------------44/48----sort-localeCompare-
+// const sortByName = users => {return [...users].sort((a, b) => a.name.localeCompare(b.name))
+// };
+
+// --------------------45/48-----Ланцюжки методів-
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Change code below this line
+// const names = [...books].filter(rat => rat.rating > MIN_BOOK_RATING)
+//   .map(authorName => authorName.author)
+//   .sort();
+
+// --------------------46/48-----Ланцюжки методів-
+// const getNamesSortedByFriendCount = users => {return [...users].sort((a, b) => a.friends.length - b.friends.length)                                              .map(user => user.name);
+// };
+
+// --------------------47/48-----Ланцюжки методів-
+// --повертала масив унікальних імен друзів (властивість friends), відсортований за алфавітом
+// const getSortedFriends = users => { return users
+//       .flatMap(user => user.friends)
+//       .filter((frend, index, array) => array.indexOf(frend) === index)
+//       .sort()
+   
+// };
+
+
+// --------------------48/48-----Ланцюжки методів-
+// --повертала загальний баланс користувачів (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender
+const getTotalBalanceByGender = (users, gender) => { return users
+      .filter(gend => gend.gender === gender)
+      .reduce((total, bal) => {return total + bal.balance;}, 0)
+
+};
