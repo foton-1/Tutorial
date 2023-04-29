1b0f8f55d9740e7ec7634339a7f19c2799901c95
foton-1 <ovf0675@outlook.com>
2023-04-29 13:42:17 +0300

    modul-5_10/20
    add 10/20

--- js/modul-5-this.js
+++ js/modul-5-this.js
@@ -1,15 +1,175 @@
-// ---------Modul-5-this.js--//
+// ---------Modul-5-this.js--this//
 // --------------1/20--
-const pizzaPalace = {
-  pizzas: ["Supercheese", "Smoked", "Four meats"],
-  checkPizza(pizzaName) {
-    return this.pizzas.includes(pizzaName);
-  },
-  order(pizzaName) {
-    const isPizzaAvailable = this.checkPizza(pizzaName);
-    if (!isPizzaAvailable) {
-      return `Sorry, there is no pizza named «${pizzaName}»`;
-    }
-    return `Order accepted, preparing «${pizzaName}» pizza`;
-  },
-};
+// const pizzaPalace = {
+//   pizzas: ["Supercheese", "Smoked", "Four meats"],
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+// };
+
+// --------------2/20--this-
+// const customer = {
+//   username: "Mango",
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ["Burger", "Pizza", "Salad"],
+//   // Change code below this line
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+//   // Change code above this line
+// };
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, "Steak");
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+// --------------3/20--this-
+// const historyService = {
+//   orders: [
+//     { email: "jacob@hotmail.com", dish: "Burrito" },
+//     { email: "solomon@topmail.net", dish: "Burger" },
+//     { email: "artemis@coldmail.net", dish: "Pizza" },
+//     { email: "solomon@topmail.net", dish: "Apple pie" },
+//     { email: "jacob@hotmail.com", dish: "Taco" },
+//   ],
+//   // Change code below this line
+//   getOrdersLog() {
+//     return this.orders
+//       .map((order) => `email: ${order.email} dish: ${order.dish}`)
+//       .join(" - ");
+//   },
+//   getEmails() {
+//     const emails = this.orders.map((order) => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter((order) => order.email === email);
+//   },
+// };
+
+// --------------4/20--Object.create(obj)-
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Change code below this line
+// const child = Object.create(parent)
+// // Change code above this line
+// child.name = "Jason";
+// child.age = 27;
+
+// --------------5/20--------ЛАНЦЮЖОК ПРОТОТИПІВ--Object.create(obj)-
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+// // Change code below this line
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// --------------6/20----Оголошений клас Car
+// class Car {}
+// console.log(Car);
+
+// --------------7/20--constructor
+// class Car {
+//   // Change code below this line
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+// console.log(new Car("Audi", "Q3", 36000));
+// // --В результаті виклику `new Car('Audi','Q3', 36000)` утвориться об'єкт ` {brand: 'Audi', model: 'Q3', price: 36000 }`
+
+// --------------8/20--патерн «Об'єкт параметрів»
+// class Car {
+//   // Change code below this line
+//   constructor({brand, model, price}) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// };
+
+// --------------9/20--методи класу
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+//   getPrice() {
+//     return this.price;
+//   }
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+// }
+
+// --------------10/20---ЗАДАЧА: СКЛАД
+/*Оголоси наступні методи класу:
+getItems() - повертає масив поточних товарів у властивості items об'єкта, який викликає цей метод.
+addItem(newItem) - приймає новий товар newItem і додає його в масив товарів у властивості items об'єкта, який викликає цей метод.
+removeItem(itemToRemove) - приймає товар itemToRemove і видаляє його з масиву товарів у властивості items об'єкта, який викликає цей метод.*/
+// class Storage {
+//   constructor(items) {
+//     this.items = ["Nanitoids", "Prolonger", "Antigravitator"];
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(newItem) {
+//     this.items.push(newItem);
+//   }
+//   removeItem(itemToRemove) {
+//     const indexItem = this.items.findIndex((item) => item === itemToRemove);
+//     if (indexItem > 0) {
+//       this.items.splice(indexItem, 1);
+//     }
+//   }
+// }
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// --------------11/20-----ЗАДАЧА: КОНСТРУКТОР РЯДКІВ
