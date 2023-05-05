54e746876c9fc2be139ee98efa11e7027e909d8e
foton-1 <ovf0675@outlook.com>
2023-05-05 22:45:37 +0300

    molul-6_
    приклад 7 з лекцій

--- js/modul-6-DOM.js
+++ js/modul-6-DOM.js
@@ -48,8 +48,78 @@
 // image.src = "https://placeimg.com/640/480/tech";
 
 // -------------4--------Властивість textContent--
-const text = document.querySelector(".article-text");
-console.log(text.textContent); // text inside p.article-text
+// const text = document.querySelector(".article-text");
+// console.log(text.textContent); // text inside p.article-text
 
-const title = document.querySelector(".article-title");
-title.textContent = "Welcome to Bahamas!";
+// const title = document.querySelector(".article-title");
+// title.textContent = "Welcome to Bahamas!";
+
+// ------------5-------Властивість classList
+/*--elem.classList.contains(cls) - повертає true або false, залежно від наявності класу cls в елемента.
+--elem.classList.add(cls) - додає клас cls до списку класів елемента.
+--elem.classList.remove(cls) - видаляє клас cls зі списку класів елемента.
+--elem.classList.toggle(cls) - якщо відсутній клас cls, то додає його, якщо - присутній, навпаки - видаляє.
+--elem.classList.replace(oldClass, newClass) - замінює існуючий клас oldClass на вказаний newClass.--*/
+// const text = document.querySelector("#paragraph");
+
+// console.log(text.classList); // ["text", "red", "big", value: "text red big"]
+// console.log(text.classList.contains("red")); // true
+
+// text.classList.remove("big");
+// console.log(text.classList); // ["text", "red", value: "text red"]
+
+// text.classList.add("new-class");
+// console.log(text.classList); // ["text", "red", "new-class", value: "text red new-class"]
+
+// // Can add multiple classes
+// text.classList.add("a", "b", "c");
+// console.log(text.classList);
+
+// text.classList.toggle("is-hidden"); // will add is-hidden class
+// text.classList.toggle("is-hidden"); // will remove is-hidden class
+
+// // classList has a forEach method
+// text.classList.forEach((cls) => {
+//   console.log(cls); // text, red, new-class
+// });
+
+//---------------6------Властивість style--не зрозумів
+// const button = document.querySelector(".btn");
+
+// button.style.backgroundColor = "teal";
+// button.style.fontSize = "24px";
+// button.style.textAlign = "center";
+
+// console.log(button.style); // inline styles object
+
+// --------------7------Атрибути
+/*elem.hasAttribute(name) - перевіряє наявність атрибута, повертає true або false.
+--elem.getAttribute(name) - отримує значення атрибута і повертає його.
+--elem.setAttribute(name, value) - встановлює атрибут.
+--elem.removeAttribute(name) - видаляє атрибут.
+--elem.attributes - властивість, що повертає об'єкт усіх атрибутів елемента--*/
+// const image = document.querySelector(".image");
+
+// console.log(image.attributes); // NamedNodeMap {0: class, 1: src, 2: alt, length: 3}
+
+// console.log(image.hasAttribute("src")); // true
+
+// console.log(image.getAttribute("alt")); // "Lake and clouds"
+
+// image.setAttribute("alt", "Amazing nature");
+
+// console.log(image.getAttribute("alt")); // Amazing nature
+
+// ---------------8--data-атрибути
+const saveBtn = document.querySelector('.editor button[data-action="save"]');
+const closeBtn = document.querySelector('.editor button[data-action="close"]');
+
+console.log(saveBtn.dataset.action); //save
+console.log(closeBtn.dataset.action); //close
+
+const dishes = document.querySelectorAll(".dishes > li");
+dishes.forEach((dish) => {
+  console.log(dish.dataset.id);
+});
+
+// --------------
