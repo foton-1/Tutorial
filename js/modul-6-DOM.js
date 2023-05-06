435f7276abd8a6d3ffd1904dd32f05e0b8704976
foton-1 <ovf0675@outlook.com>
2023-05-06 22:45:45 +0300

    mod-6_ex-12

--- js/modul-6-DOM.js
+++ js/modul-6-DOM.js
@@ -1,5 +1,5 @@
-// -----------Лекція--
-// -----------1------Навігація по DOM--
+// -----------Конеспект-6/11
+// -----------Ех-1------Навігація по DOM--
 // console.log(document);
 
 // const body = document.body;
@@ -14,7 +14,7 @@
 // const listItems = list.children;
 // console.log(listItems);
 
-// ----------2-------Пошук елементів--
+// ---------Ех-2-------Пошук елементів--
 // const listWithId = document.querySelector("#menu");
 // listWithId.style.textTransform = "uppercase";
 // listWithId.style.fontSize = "24px";
@@ -33,7 +33,7 @@
 // firstMenuItem.style.color = "tomato";
 // console.log(firstMenuItem);
 
-// --------3-----------Властивості та атрибути--
+// -------Ех-3-----------Властивості та атрибути--
 //===================================================
 // const message = document.querySelector("#message");
 // console.log(message.value); // Default textarea message
@@ -47,14 +47,14 @@
 // console.log(image.src); // https://placeimg.com/640/480/animals
 // image.src = "https://placeimg.com/640/480/tech";
 
-// -------------4--------Властивість textContent--
+// ------------Ех-4--------Властивість textContent--
 // const text = document.querySelector(".article-text");
 // console.log(text.textContent); // text inside p.article-text
 
 // const title = document.querySelector(".article-title");
 // title.textContent = "Welcome to Bahamas!";
 
-// ------------5-------Властивість classList
+// -----------Ех-5-------Властивість classList
 /*--elem.classList.contains(cls) - повертає true або false, залежно від наявності класу cls в елемента.
 --elem.classList.add(cls) - додає клас cls до списку класів елемента.
 --elem.classList.remove(cls) - видаляє клас cls зі списку класів елемента.
@@ -83,7 +83,7 @@
 //   console.log(cls); // text, red, new-class
 // });
 
-//---------------6------Властивість style--не зрозумів
+//--------------Ех-6------Властивість style--не зрозумів
 // const button = document.querySelector(".btn");
 
 // button.style.backgroundColor = "teal";
@@ -92,7 +92,7 @@
 
 // console.log(button.style); // inline styles object
 
-// --------------7------Атрибути
+// -------------Ех-7------Атрибути
 /*elem.hasAttribute(name) - перевіряє наявність атрибута, повертає true або false.
 --elem.getAttribute(name) - отримує значення атрибута і повертає його.
 --elem.setAttribute(name, value) - встановлює атрибут.
@@ -110,16 +110,86 @@
 
 // console.log(image.getAttribute("alt")); // Amazing nature
 
-// ---------------8--data-атрибути
-const saveBtn = document.querySelector('.editor button[data-action="save"]');
-const closeBtn = document.querySelector('.editor button[data-action="close"]');
+// --------------Ех-8--data-атрибути--dataset
+/*--Для отримання значення data-атрибута використовується властивість 
+--dataset, після якого стоїть ім'я атрибута.--*/
+// const saveBtn = document.querySelector('.editor button[data-action="save"]');
+// const closeBtn = document.querySelector('.editor button[data-action="close"]');
+// console.log(saveBtn.dataset.action); //save
+// console.log(closeBtn.dataset.action); //close
 
-console.log(saveBtn.dataset.action); //save
-console.log(closeBtn.dataset.action); //close
+// const dishes = document.querySelectorAll(".dishes > li");
+// dishes.forEach((dish) => {
+//   console.log(dish.dataset.id);
+// });
 
-const dishes = document.querySelectorAll(".dishes > li");
-dishes.forEach((dish) => {
-  console.log(dish.dataset.id);
-});
+// -------------Ех-9-Створення--createElement()-
+/*-document.createElement(tagName)-Створює елемент з ім'ям tagName і повертає посилання на нього як результат 
+--свого виконання. tagName - це рядок, що вказує тип елемента, який створюється --*/
+// const heading = document.createElement("h1");
+// console.log(heading); // <h1></h1>
 
-// --------------
+// heading.textContent = "This is a heading";
+// console.log(heading); // <h1>This is a heading</h1>
+
+// const image = document.createElement("img");
+// image.src = "https://placeimg.com/640/480/nature";
+// image.alt = "Nature";
+// console.log(image); // <img src="https://placeimg.com/640/480/nature" alt="Nature" />
+
+// --------------Ех-10-Додавання
+/*--Щоб створений елемент відображався на сторінці, його необхідно додати до 
+--вже існуючого елемента в DOM-дереві. 
+--element.append(el1, el2, ...) - додає один або декілька елементів після всіх дітей елемента element.
+--element.prepend(el1, el2, ...) - додає один або декілька елементів перед усіма дітьми елемента element.
+--element.after(el1, el2, ...) - додає один або декілька елементів після елемента element.
+--element.before(el1, el2, ...) - додає один або декілька елементів перед елементом element.
+-*/
+// const list = document.querySelector(".usernames");
+
+// // Adds an item to the end of the list
+// const lastItem = document.createElement("li");
+// lastItem.textContent = "Poly";
+// list.append(lastItem);
+
+// // Adds an item to the beginning of the list
+// const firstItem = document.createElement("li");
+// firstItem.textContent = "Ajax";
+// list.prepend(firstItem);
+
+// // Adds a title before the list
+// const title = document.createElement("h2");
+// title.textContent = "USERNAMES";
+// list.before(title);
+
+// // Adds a paragraph after the list
+// const text = document.createElement("p");
+// text.textContent =
+//   "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime, soluta aspernatur quasi nihil in asperiores ad distinctio illo et debitis error iure voluptate numquam maiores nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime, soluta aspernatur quasi nihil in asperiores ad distinctio illo et debitis error iure voluptate numquam maiores nisi!";
+// list.after(text);
+
+// --------------Ex-11-Видалення-elem.remove();
+// const text = document.querySelector(".text");
+// text.remove();
+
+// --------------Ex-12-Властивість innerHTML
+/*--Читання. Властивість innerHTML зберігає вміст елемента, включно з тегами, у вигляді рядка.
+----------12a--Зміна. Властивість innerHTML доступна і для читання, і для запису.*/
+// const title = document.querySelector(".article .title");
+// title.innerHTML = 'New and <span class="accent">improved</span> title';
+
+// ---------12b--Однотипна (шаблонна) розмітка створюється із масиву даних.
+// const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+// const list = document.querySelector(".list");
+
+// const markup = technologies
+//   .map((technology) => `<li class="list-item">${technology}</li>`)
+//   .join("");
+
+// // Check the console, you'll see a single string with HTML tags
+// console.log(markup);
+
+// // Adding all the markup in one operation
+// list.innerHTML = markup;
+
+// 
